2d42136546e63b73aff0e394d38af132f09738b6
GuyCalloway <guyfcalloway@gmail.com>
2019-11-12 19:41:48 +0000

    Setting up Jest

---
+++ functions.test.js
@@ -0,0 +1,17 @@
+const functions = require('./functions.js');
+
+test("adds 2+2 to Equal 4", () => {
+    expect(functions.add(2, 2)).toBe(4);
+});
+
+test("isNull function returns isNull", () => {
+    expect(functions.isNull()).toBe(null);
+});
+
+test("value check 4 to be 4", () => {
+    expect(functions.checkValue(4)).toBe(4);
+});
+
+test("knows if falsy value passed", () => {
+    expect(functions.checkValue(0)).toBeFalsy();
+});
